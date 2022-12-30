e3b95a29e683b6f4b55da67a38bea584f089e136
David Emrich <69828479+dvieras@users.noreply.github.com>
2022-12-30 20:57:32 -0300

    teste de declaracao de variaveis e concatenacao (#1)

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,31 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // tipo primitivos:
+
+        String nome = "David";
+        char sexo = 'M';
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 99999999999L; // Utilizar 'L' no final do valor atribuido;
+        float pi = 3.14F; // Utilizar 'F' no final do valor atribuido;
+        double salario = 1275.33;
+        boolean verdadeFalso = true;
+        
+        //testes
+
+        int numero = 5;
+        System.out.println(numero);
+        numero = 10;
+        System.out.println(numero);
+
+        String primeiroNome = "David";
+        String segundoNome = "Emrich";
+        String nomeCompleto;
+        nomeCompleto = primeiroNome.concat(" ").concat(segundoNome);
+
+        System.out.println(nomeCompleto);
+
     }
 }
